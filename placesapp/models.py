3a082b4ace514c0c4cc787b57dc75307ef827118
karthikraja34 <rajakarthik131@gmail.com>
2018-07-30 13:49:40 +0530

    Add model in placesapp
    Created new model to store place type and linked with Place model as many to many field

--- placesapp/models.py
+++ placesapp/models.py
@@ -2,6 +2,13 @@ from django.contrib.gis.db import models
 from taggit.managers import TaggableManager
 
 
+class PlaceType(models.Model):
+    place = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
 class Place(models.Model):
     title = models.CharField(max_length=50)
     location = models.PointField()
@@ -10,3 +17,4 @@ class Place(models.Model):
     phone = models.IntegerField()
     city = models.CharField(max_length=50)
     tags = TaggableManager()
+    places = models.ManyToManyField("PlaceType")
